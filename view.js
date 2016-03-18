0997722e6ace6ab07613cadbd295b250807b20d3
Kang Chen <kang@airtasker.com>
2016-03-18 18:10:17 +1100

    TEXT pass

--- view.js
+++ view.js
@@ -44,6 +44,7 @@ let view = ()=> {
 
   let load = () => {
 		scene.remove(object);
+		scene.remove(object);
 
 		light.intensity = 0.3;
 		object = new THREE.Object3D();
@@ -97,16 +98,114 @@ let view = ()=> {
 
 	}
 
-  let addObject = (ev, obj) => {
+	function makeTextSprite( message, parameters )
+	{
+		console.log("maketextsprite")
+		if ( parameters === undefined ) parameters = {};
+
+		var fontface = parameters.hasOwnProperty("fontface") ?
+			parameters["fontface"] : "Arial";
+
+		var fontsize = parameters.hasOwnProperty("fontsize") ?
+			parameters["fontsize"] : 18;
+
+		var borderThickness = parameters.hasOwnProperty("borderThickness") ?
+			parameters["borderThickness"] : 4;
+
+		var borderColor = parameters.hasOwnProperty("borderColor") ?
+			parameters["borderColor"] : { r:0, g:0, b:0, a:1.0 };
+
+		var backgroundColor = parameters.hasOwnProperty("backgroundColor") ?
+			parameters["backgroundColor"] : { r:255, g:255, b:255, a:1.0 };
+
+		// var spriteAlignment = THREE.SpriteAlignment.topLeft;
+
+		var canvas = document.createElement('canvas');
+		var context = canvas.getContext('2d');
+		context.font = "Bold " + fontsize + "px " + fontface;
+
+		// get size data (height depends only on font size)
+		var metrics = context.measureText( message );
+		var textWidth = metrics.width;
+
+		// background color
+		context.fillStyle   = "rgba(" + backgroundColor.r + "," + backgroundColor.g + ","
+									  + backgroundColor.b + "," + backgroundColor.a + ")";
+		// border color
+		context.strokeStyle = "rgba(" + borderColor.r + "," + borderColor.g + ","
+									  + borderColor.b + "," + borderColor.a + ")";
+
+		context.lineWidth = borderThickness;
+		roundRect(context, borderThickness/2, borderThickness/2, textWidth + borderThickness, fontsize * 1.4 + borderThickness, 6);
+		// 1.4 is extra height factor for text below baseline: g,j,p,q.
+
+		// text color
+		context.fillStyle = "rgba(0, 0, 0, 1.0)";
+
+		context.fillText( message, borderThickness, fontsize + borderThickness);
+
+		// canvas contents will be used for a texture
+		var texture = new THREE.Texture(canvas)
+		texture.needsUpdate = true;
+
+		var spriteMaterial = new THREE.SpriteMaterial(
+			{ map: texture, color:  0xffffff} );
+		var sprite = new THREE.Sprite( spriteMaterial );
+		sprite.scale.set(100,50,1.0);
+		return sprite;
+	}
+
+	// function for drawing rounded rectangles
+	function roundRect(ctx, x, y, w, h, r)
+	{
+	    ctx.beginPath();
+	    ctx.moveTo(x+r, y);
+	    ctx.lineTo(x+w-r, y);
+	    ctx.quadraticCurveTo(x+w, y, x+w, y+r);
+	    ctx.lineTo(x+w, y+h-r);
+	    ctx.quadraticCurveTo(x+w, y+h, x+w-r, y+h);
+	    ctx.lineTo(x+r, y+h);
+	    ctx.quadraticCurveTo(x, y+h, x, y+h-r);
+	    ctx.lineTo(x, y+r);
+	    ctx.quadraticCurveTo(x, y, x+r, y);
+	    ctx.closePath();
+	    ctx.fill();
+		ctx.stroke();
+	}
+
+  let addObjectPusher = (ev, obj) => {
+
 		var geometry = new THREE.SphereGeometry( Math.random() + 0.9, Math.random() + 6, Math.random()  );
 		var material = new THREE.MeshPhongMaterial( { color: 0xffffff, shading: THREE.FlatShading } );
 		var mesh = new THREE.Mesh( geometry, material );
-		mesh.position.set( Math.random() - 0.5, Math.random() - 0.6, Math.random() - 0.5 ).normalize();
+		var spherePositionX =  Math.random() - 0.5;
+		var spherePositionY =  Math.random() - 0.6;
+		var spherePositionZ =  Math.random() - 0.5;
+		mesh.position.set( spherePositionX, spherePositionY, spherePositionZ ).normalize();
 		mesh.position.multiplyScalar( Math.random() * 300 );
-		mesh.rotation.set( Math.random() * 3, Math.random() * 3, Math.random() * 3 );
+		var sphereRotationX =  Math.random() * 3;
+		var sphereRotationY =  Math.random() * 3;
+		var sphereRotationZ =  Math.random() * 3;
+		mesh.rotation.set( sphereRotationX, sphereRotationY, sphereRotationZ);
 		mesh.scale.x = mesh.scale.y = mesh.scale.z = Math.random() * 46;
 		object.add( mesh );
+
+		if (typeof obj != "undefined") {
+			if (typeof obj.message != "undefined") {
+				var spritey = makeTextSprite( obj.message.title,
+				{ fontsize: 32, fontface: "Georgia", borderColor: {r:0, g:0, b:255, a:1.0} } );
+				spritey.position.set(spherePositionX,spherePositionY,spherePositionZ);
+				object.add( spritey );
+			}
+		}
+
+		// spherePositionX = 0;
+		// sphereRotationY = 0;
+		// sphereRotationZ = 0;
+
   }
+
+
 
 	var camera, scene, renderer, composer;
 	var object, light, light2, ambientLight;
@@ -122,7 +221,7 @@ let view = ()=> {
 
   let otherLog = (ev, status) => {
     console.log(ev, status);
-    addObject(ev, status);
+    addObjectPusher(ev, status);
   }
 
   return {
